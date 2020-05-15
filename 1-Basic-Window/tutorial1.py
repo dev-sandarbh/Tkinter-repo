ec8276045f0d64d6ec99643755286cab339d36e9
_sandarbhTiwari <tiwari.sandarbh2603@gmail.com>
2020-05-15 18:03:16 +0530

    First Commit. First Tkinter Tutorial.

---
+++ 1-Basic-Window/tutorial1.py
@@ -0,0 +1,11 @@
+''' in this tutorial we will learn about Tkinter Module which comes pre-bundled with the python programming language for developing Graphical User Interface(GUI). So let us start... with the very basic code first and then we move to some more advanced and complex stuffs in tkinter as well.'''
+# first import tkinter library
+import tkinter
+
+# make an object of the tkinter class;as you know everything in python is a 'class'
+# let us name our object as root(for main-window)
+root = tkinter.Tk()
+
+
+# to make our app functional and running, we call an infinte loop which keeps our app on the screen unless you don't quit it
+root.mainloop() 
